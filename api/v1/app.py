d698feb90bc1d8a6f4bbddf80f186a3b0cc75ff7
Koketso-dax <lanchaster.k@gmail.com>
2024-04-27 12:07:44 +0200

    Fix pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,10 +7,12 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Close storage"""
     storage.close()
+
 
 if __name__ == "__main__":
     """Main loop program"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     """Returns status OK"""
