ef19f2659abb87443ebd44a613eeab6cc1431666
Koketso-dax <lanchaster.k@gmail.com>
2024-04-27 12:04:13 +0200

    api status

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+"""Main App module for v1 api."""
+import os
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """Close storage"""
+    storage.close()
+
+if __name__ == "__main__":
+    """Main loop program"""
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+"""Return OK if api is running with no issue."""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    """Returns status OK"""
+    return jsonify({"status": "OK"})
