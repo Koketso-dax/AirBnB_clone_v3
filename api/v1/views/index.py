abfcd2fbd5e09eaa8df2e0c84b9a7772def47451
Koketso-dax <lanchaster.k@gmail.com>
2024-04-28 13:30:37 +0200

    include get count

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,20 @@
 """Return OK if api is running with no issue."""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns status OK"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_count():
+    """Retrieves number of each obj type in storage"""
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
