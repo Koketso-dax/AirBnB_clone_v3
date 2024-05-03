8f33ae6a4b73005857e83690aafbbbd2d8cf222a
Koketso-dax <lanchaster.k@gmail.com>
2024-05-03 16:47:13 +0200

    put problems in state

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -82,15 +82,15 @@ def put_state(state_id):
     if not state:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
     if request.content_type != 'application/json':
-        abort(400, description='Not a JSON')
+        abort(400)
+
+    data = request.get_json()
+    if not data:
+        abort(400, description="Not a JSON")
 
     ignore = ['id', 'created_at', 'updated_at']
 
-    data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
             setattr(state, key, value)
